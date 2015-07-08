3bafcaab40c443d1f74397b0b2bebfbf09fd610e
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2015-07-08 15:18:40 +0200

    Global

--- soweb/Global.asax.cs
+++ soweb/Global.asax.cs
@@ -8,7 +8,6 @@ namespace Soweb
 {
     public class Global : HttpApplication
     {
-
         protected void Application_Start(object sender, EventArgs e)
         {
             AreaRegistration.RegisterAllAreas();
